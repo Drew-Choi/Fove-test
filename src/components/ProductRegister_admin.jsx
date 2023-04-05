17191640ed7cef56b1726270e462d351f86ea4d4
Drew <drc920630@gmail.com>
2023-04-06 03:18:44 +0900

    axios오류 남

--- src/components/ProductRegister_admin.jsx
+++ src/components/ProductRegister_admin.jsx
@@ -1,27 +1,66 @@
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React, { useEffect, useRef, useState } from 'react';
+import '../styles/productRegister_admin.scss';
+import axios from 'axios';
 
 export default function ProductRegister_admin() {
-  return (
-    <>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>상품명</Form.Label>
-          <Form.Control type="text" placeholder="상품명" />
-          <Form.Text className="text-muted">...</Form.Text>
-        </Form.Group>
+  const [newProduct, setNewProduct] = useState({});
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </>
+  const pd_name = useRef();
+  const pd_price = useRef();
+  const pd_quantity = useRef();
+
+  const onChangeNewProduct = () => {
+    let addPoductObj = {
+      name: pd_name.current.value,
+      price: pd_price.current.value,
+      quantity: pd_quantity.current.value,
+    };
+    setNewProduct((cur) => addPoductObj);
+  };
+
+  useEffect(() => {
+    const res = async () =>
+      await axios.post('http://localhost4000/pd_register/add_pd', {
+        name: newProduct.name,
+        price: newProduct.price,
+        quantity: newProduct.quantity,
+      });
+    res();
+    console.log(res);
+  }, []);
+
+  return (
+    <section>
+      <div className="register_container">
+        <div>
+          <p>상품명</p>
+          <input
+            ref={pd_name}
+            type="text"
+            name="name"
+            placeholder="상품이름을 입력해주세요"
+          />
+        </div>
+        <div>
+          <p>가격&nbsp;&nbsp;&nbsp;</p>
+          <input
+            ref={pd_price}
+            type="number"
+            name="price"
+            placeholder="가격을 입력해주세요"
+          />
+        </div>
+        <div>
+          <p>수량&nbsp;&nbsp;&nbsp;</p>
+          <input
+            ref={pd_quantity}
+            type="number"
+            name="quantity"
+            placeholder="재고수량을 입력해주세요"
+          />
+        </div>
+        <button onClick={onChangeNewProduct}>등록</button>
+      </div>
+    </section>
   );
 }
